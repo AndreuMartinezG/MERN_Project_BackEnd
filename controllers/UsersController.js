952fb9599b37680b1f187341b8ea165e17b7c48e
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-23 20:34:20 +0100

    feat: Funcion para añadir al array de followers amigos nuevos

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,6 +2,7 @@ const UsersController = {};
 const authConfig = require('../config/auth');
 const User = require('../models/user.js');
 const bcrypt = require('bcrypt');
+const { use } = require('bcrypt/promises');
 
 //Registro de usuarios
 UsersController.userRegister = async (req, res) => {
@@ -13,15 +14,15 @@ UsersController.userRegister = async (req, res) => {
     let email = req.body.email;
     let userName = req.body.userName;
     let password = bcrypt.hashSync(req.body.password, Number.parseInt(authConfig.rounds));
- 
+
 
     //Comprobación de errores.....
     User.find({
         email: email
     }).then(datosRepetidos => {
-        
+
         if (datosRepetidos == false) {
-            console.log("ESTOY AQUI")
+            
             User.create({
                 firstName: firstName,
                 lastName: lastName,
@@ -53,32 +54,67 @@ UsersController.userFollow = async (req, res) => {
 
     let id_follower = req.body.id_follower
 
-    User.find({
-        _id: _id
-    }).then(userFind => {
-        
-        if (userFind == true) {
-            try{
-
-                User.followers.update(
-                    { _id: _id }, 
-                    { $push: { id_follower: id_follower } },
-                    done
-                );
-
-            }catch(error){
-                res.send(error)
+    // Enviar Mensaje al usuario que ya sigue a esa persona
+    try {
+        console.log("entramos aqui")
+        User.findOneAndUpdate(
+            { _id: _id},
+            {
+                $push: {
+                    followers: {
+                        "_id": _id,
+                        "id_follower": id_follower
+                    }
+                }
             }
+            
+        )
+        console.log("PasamosPush")
+        res.send("Correcto")
 
-        } else {
-            res.send("El usuario con ese e-mail ya existe en nuestra base de datos");
-        }
-    }).catch(error => {
+    } catch (error) {
         res.send(error)
-    });
+    }
+
+
+
 }
 
 
 
 
 module.exports = UsersController;
+
+
+
+
+
+
+
+
+
+// User.find({
+//     _id: _id
+// }).then(userFind => {
+//     if ("623b59aa3f4be1962d582f5f" === "623b59aa3f4be1962d582f5f") {
+
+//         console.log("entramos if")
+//         try{
+//             console.log("Entramos a update")
+//             User.followers.updateOne(
+//                 { _id: _id },
+//                 { $push: [{ id_follower: id_follower}] },
+//                 res.send("Hemos pasado el update")
+//             );
+
+//         }catch(error){
+//             res.send(error)
+//         }
+
+//     } else {
+//         res.send("la id del usuario a seguir no existe");
+//     }
+
+// }).catch(error => {
+//     res.send(error)
+// });
