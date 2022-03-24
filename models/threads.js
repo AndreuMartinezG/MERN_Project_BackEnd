2d3729ddf9bff586cb2696d323efbd4a05ca78fa
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-24 19:52:26 +0100

    fix: error al añadir el nuevo archivo de threads/model

---
+++ models/threads.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const { type } = require('os')
+
+const Schema = mongoose.Schema
+
+
+const threadSchema = new Schema({
+    id_owner: {
+        type: String,
+        required: true
+    },
+    userName_owner: {
+        type: String,
+        required: true
+    },
+    headLine: {
+        type: String,
+        required: true,
+    },
+    post: [
+
+        {
+            id_owner: String,
+            userName_owner: String,
+            headLine_post: String,
+            text_post: Text,
+            likes : Number,
+            dislikes : Number,
+            created_post: {
+                type: Date,
+                default: new Date()
+            },
+        }
+    ],
+    created: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+const Thread = mongoose.model('Tread', threadSchema)
+module.exports = Thread
